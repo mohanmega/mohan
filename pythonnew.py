d3de3224202971051178476f0c9c262206160a45
sundar <sundarshivajii@gmail.com>
2023-08-30 15:53:40 +0530

    new folder

---
+++ pythonnew.py
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" type="text/css" href="login.css">
+</head>
+<body>
+    <div class="box">
+        <form action="home.html">
+            <h2><b>Sign in</b></h2>
+            <div class="inputbox">
+                <input type="text" required="required">
+                <span>Username</span>
+                <i></i>
+            </div>
+            <div class="inputbox">
+                <input type="password" reqired="reqired">
+                <span>Password</span>
+                <i></i>
+            </div>
+
+
+            <div class="links">
+                <a href="#">Forget Password</a>
+                <a href="signup.html"><b>Sign Up</b></a>
+            </div>
+
+            <input type="submit" value="login">
+        </form>
+    </div>
+</body>
+</html>
